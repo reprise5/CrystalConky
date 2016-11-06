d6d2e25d46f92eb2723539ef2437177fa23fd2b7
reprise5 <rzqxx5gv@gmail.com>
2016-11-06 00:08:05 -0400

    added more monitors to module 'right'.

--- config/startconky.sh
+++ config/startconky.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+#Gets the directory you put the modules to conky in, so that thi script's working directory
+#is in the relevant place.
 current="$(readlink -f $(dirname "$0"))"
 previous="$(pwd)"
 cd "$current"
 
-conky -c ./right &
-
+conky -c ./right #&  if adding more modules, unhash the & symbol, and add the names of your modules below.
+#conky -c ./nameOfModule
+#conky -c ./temp-rc   <<integrated into right-rc.  But you can use this module seperqtely if you'd like. just uncomment it.'
