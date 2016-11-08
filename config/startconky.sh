bf02154bb65e92ebbc75712829ac19021d2a691e
reprise5 <rzqxx5gv@gmail.com>
2016-11-07 21:50:31 -0500

    color options, fixed username heading

--- config/startconky.sh
+++ config/startconky.sh
@@ -6,6 +6,11 @@ current="$(readlink -f $(dirname "$0"))"
 previous="$(pwd)"
 cd "$current"
 
-conky -c ./right #&  if adding more modules, unhash the & symbol, and add the names of your modules below.
 #conky -c ./nameOfModule
 #conky -c ./temp-rc   <<integrated into right-rc.  But you can use this module seperqtely if you'd like. just uncomment it.'
+#pick which you want by unhashing it.
+conky -c ./right #&  if adding more modules AT ONCE, unhash the & symbol, and add the names of your modules below.
+#conky -c ./right_light
+#conky -c ./right_dark
+#conky -c ./right-short
+
